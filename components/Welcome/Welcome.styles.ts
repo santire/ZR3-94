d53dedd2d5689f57b32df7da55156c3bf97eefec
Santiago Reyes <sreyes@itba.edu.ar>
2022-08-03 20:59:54 -0300

    responsive landing page

--- components/Welcome/Welcome.styles.ts
+++ components/Welcome/Welcome.styles.ts
@@ -1,6 +1,6 @@
 import { createStyles } from '@mantine/core';
 
-export default createStyles((_) => ({
+export default createStyles((theme) => ({
   container: {
     display: 'flex',
     flexDirection: 'column',
@@ -11,10 +11,43 @@ export default createStyles((_) => ({
   content: {
     display: 'flex',
     margin: '0 auto',
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      flexDirection: 'column',
+    },
+  },
+  title: {
+    textAlign: 'left',
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      textAlign: 'center',
+    },
+  },
+  text: {
+    fontSize: 24,
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      flexDirection: 'column',
+      fontSize: 18,
+    },
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      flexDirection: 'column',
+      fontSize: 14,
+    },
   },
   link: {
-    fontSize: 22,
     letterSpacing: 8,
+    fontSize: 22,
     color: 'yellowgreen',
+    [`@media (max-width: ${theme.breakpoints.md}px)`]: {
+      flexDirection: 'column',
+      fontSize: 16,
+    },
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      flexDirection: 'column',
+      fontSize: 12,
+    },
+  },
+  image: {
+    [`@media (max-width: ${theme.breakpoints.sm}px)`]: {
+      margin: '0 auto',
+    },
   },
 }));
--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,21 +1,30 @@
 import { Title, Text, Anchor, Image, Container } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import Link from 'next/link';
 import useStyles from './Welcome.styles';
 
 export function Welcome() {
   const { classes } = useStyles();
+  const largeScreen = useMediaQuery('(min-width: 1420px)');
 
   return (
     <div className={classes.container}>
       <div className={classes.content}>
-        <Image src="assets/logo.png" alt="logo" width="11em" height="11em" p={0} />
+        <Image
+          className={classes.image}
+          src="assets/logo.png"
+          alt="logo"
+          width="11em"
+          height="11em"
+          p={0}
+        />
         <Container m={0} pt="3em">
-          <Title align="left" order={3}>
+          <Title className={classes.title} order={largeScreen ? 3 : 4}>
             INVESTIGADORES
             <br />
             DE ANOMALÍAS TRASCENDENTALES
           </Title>
-          <Text align="justify" size={24} mx="auto" mt="xl" mb="2em">
+          <Text align="justify" mx="auto" mt="xl" mb="2em">
             Somos Investigadores de Anomalías Trascendentales (I.A.T), una agrupación integrada por
             investigadores desertores de grandes y confidenciales estudios sobre avances
             tecnológicos. Nuestro objetivo es advertir a la humanidad sobre ZR3-94 y dar cuenta de
@@ -26,11 +35,13 @@ export function Welcome() {
         </Container>
       </div>
 
-          <Link href="/home" passHref>
-            <Anchor className={classes.link} component="a">
-              <Text align="center">Accedé a toda la información</Text>
-            </Anchor>
-          </Link>
+      <Link href="/home" passHref>
+        <Anchor className={classes.link} component="a">
+          <Text align="center">
+            Accedé a toda la información
+          </Text>
+        </Anchor>
+      </Link>
     </div>
   );
 }
