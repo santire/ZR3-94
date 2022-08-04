00d88b538afc2ee9fadf9538f26fccaca50b2ee0
Santiago Reyes <sreyes@itba.edu.ar>
2022-08-03 23:16:33 -0300

    added simple glitch

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,9 +1,28 @@
 import { Title, Text, Anchor, Image, Container } from '@mantine/core';
 import Link from 'next/link';
+import { useState } from 'react';
 import useStyles from './Welcome.styles';
 
 export function Welcome() {
   const { classes } = useStyles();
+  const textWords =
+    `Somos Investigadores de Anomalías Trascendentales (I.A.T), una agrupación integrada por
+            investigadores desertores de grandes y confidenciales estudios sobre avances
+            tecnológicos. Nuestro objetivo es advertir a la humanidad sobre ZR3-94 y dar cuenta de
+            los importantes acontecimientos que están occuriendo en el mundo (como consecuencia del
+            desarrollo del mismo) y que los gobiernos de mayor poder, encabezados por los Estados
+Unidos, quieren mantener ocultos.`
+      .split(' ')
+      .filter((e) => !!e);
+  const [paintedCount, setPaintedCount] = useState(textWords.length);
+
+  const spanMouseOverHandler = (event: React.MouseEvent<HTMLSpanElement>) => {
+    const word: HTMLSpanElement = event.currentTarget;
+    if (word.style.color != 'greenyellow' && Math.random() <= 0.05) {
+      word.style.color = 'greenyellow';
+      setPaintedCount((oldCount) => oldCount - 1);
+    }
+  };
 
   return (
     <div className={classes.container}>
@@ -15,26 +34,36 @@ export function Welcome() {
           width="11em"
           height="11em"
           p={0}
+          hidden={paintedCount <= 0}
         />
         <Container m={0} pt="3em">
-          <Title className={classes.title} order={3}>
+          <Title className={classes.title} order={3} hidden={paintedCount <= 0}>
             INVESTIGADORES
             <br />
             DE ANOMALÍAS TRASCENDENTALES
           </Title>
-          <Text className={classes.text} align="justify" mx="auto" mt="xl" mb="2em">
-            Somos Investigadores de Anomalías Trascendentales (I.A.T), una agrupación integrada por
-            investigadores desertores de grandes y confidenciales estudios sobre avances
-            tecnológicos. Nuestro objetivo es advertir a la humanidad sobre ZR3-94 y dar cuenta de
-            los importantes acontecimientos que están occuriendo en el mundo (como consecuencia del
-            desarrollo del mismo) y que los gobiernos de mayor poder, encabezados por los Estados
-            Unidos, quieren mantener ocultos.
-          </Text>
+          {paintedCount > 0 ? (
+            <Text align="justify" mx="auto" mt="xl" mb="2em">
+              {textWords.map((w, i) => (
+                <span
+                  className={classes.text}
+                  id={`w${i}`}
+                  key={i}
+                  onMouseOver={spanMouseOverHandler}
+                >
+                  {w}{' '}
+                </span>
+              ))}
+            </Text>
+          ) : (
+            <Text size={128} align="justify" mx="auto" mt="2em" mb="2em">
+              te la creiste xd
+            </Text>
+          )}
         </Container>
       </div>
-
       <Link href="/home" passHref>
-        <Anchor className={classes.link} component="a">
+        <Anchor className={classes.link} component="a" hidden={paintedCount <= 0}>
           <Text align="center">Accedé a toda la información</Text>
         </Anchor>
       </Link>
