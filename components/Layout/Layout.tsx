96f3c339653122a5fb0f9d04b009fadfbd4619c1
Santiago Reyes <sreyes@itba.edu.ar>
2022-09-05 20:20:54 -0300

    added layout component

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import { Header } from "../Header/Header";
+
+interface LayoutProps {
+  children: React.ReactNode;
+  meta: { title: string, description: string, icon?: string }
+}
+export default function Layout({ children, meta }: LayoutProps) {
+  const { title, description, icon } = meta
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href={icon || "favicon.ico"} />
+      </Head>
+      <main>
+        <Header />
+        {children}
+      </main>
+
+    </>
+  )
+}
--- pages/home.tsx
+++ pages/home.tsx
@@ -1,9 +1,14 @@
-import { Header } from '../components/Header/Header';
+import Layout from '../components/Layout/Layout';
 
 export default function HomePage() {
   return (
     <>
-      <Header></Header>
+      <Layout meta={{
+        title: "ZR3-94",
+        description: "Página principal de los investigadores de anomalías trascendentales"
+      }}>
+        <h1> hola </h1>
+      </Layout>
     </>
   );
 }
