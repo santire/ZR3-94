31cf98f02504c6dd28be4ad890fea3f847afe5db
Santiago Reyes <sreyes@itba.edu.ar>
2022-09-06 23:25:35 -0300

    added aleph redirect

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,8 +1,18 @@
-import { Autocomplete, TextInput } from '@mantine/core';
+import { Autocomplete } from '@mantine/core';
 import { IconSearch } from '@tabler/icons';
+import { useEffect, useState } from 'react';
 export function Search() {
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    if (value === 'aleph')
+      window.location.href = 'https://docs.google.com/viewer?a=v&pid=sites&srcid=ZGVmYXVsdGRvbWFpbnxiaWJsaWJyb3NwZGZ8Z3g6NWUyOTU3NzQxYmM3NWM1Yg'
+  }, [value])
+
   return (
     <Autocomplete
+      value={value}
+      onChange={setValue}
       icon={<IconSearch size={20} stroke={2} color="black" />}
       variant="filled"
       radius="xl"
@@ -10,7 +20,7 @@ export function Search() {
       height={40}
       data={['no', 'hay', 'nada', 'todavia']}
       styles={{
-        input: { background: '#D9D9D9', minWidth: 324 },
+        input: { background: '#D9D9D9', minWidth: 324, color: "black" },
       }}
     />
   );
